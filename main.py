d55b96e3feb6a95059882f8a9bb4dd74a14c6858
David Nevezi Strango <david.nevezi00@e-uvt.ro>
2024-02-01 18:21:53 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,99 @@
+import cv2
+import imutils
+import numpy as np
+import tensorflow as tf
+from scipy import ndimage
+import datetime
+# from model import trainModel
+# document_img = cv2.imread("id/id5.jpg")
+# document_img = imutils.resize(document_img, width=460)
+
+def rotateImg(img):
+    src = imutils.resize(img, width=460)
+    #hough transform, tried before, gives sometimes perpendicular images
+    gray = cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((5,5),np.float32)/25
+    gray = cv2.filter2D(gray,-1,kernel)
+    edges = cv2.Canny(gray,400,600,apertureSize = 5)
+    # cv2.imshow('image',edges)
+    # cv2.waitKey(0)
+
+    lines = cv2.HoughLines(edges,1,np.pi/180,15)
+    angles = list()
+    for i in range(8):
+        for rho,theta in lines[i]:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*(a))
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*(a))
+            angle = np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi
+            angles.append(angle)
+            # cv2.line(src, (x1, y1), (x2, y2), (0, 0, 255), 3)
+
+    # Rotate image to be horizontal
+    # Using median angle due to inconsistency of dominant line detection
+    median_angle = np.median(angles)
+    rotated_img = ndimage.rotate(src, median_angle)
+
+    # for some reason crop breaks the live detection
+    # # Crop image to retain original resolution
+    # h_rot, w_rot = rotated_img.shape[:2]
+    # h, w = src.shape[:2]
+    # dist_w, dist_h = (w // 2, h // 2)
+    # center_rot = (w_rot // 2, h_rot // 2)
+    # cropped_img = rotated_img[center_rot[0] - dist_h: center_rot[0] + dist_h,
+    #               center_rot[1] - dist_w: center_rot[1] + dist_w]
+
+    # # Display original image with the hough lines drawn
+    # cv2.imshow("Detected lines", src)
+    # # cv2.imwrite("detected.jpg", src)
+    # cv2.waitKey(0)
+    # # Display the rotated image
+    # cv2.imshow("Rotated Image", rotated_img)
+    # # cv2.imwrite("rotated.jpg", rotated_img)
+    # cv2.waitKey(0)
+    # # Display the cropped image
+    # cv2.imshow("Cropped Image", cropped_img)
+    # # cv2.imwrite("cropped.jpg", cropped_img)
+    # cv2.waitKey(0)
+
+    return rotated_img
+
+
+def evaluate():
+    cap = cv2.VideoCapture(0)
+    model = tf.keras.models.load_model("mobilenetv2ID.keras")
+    while True:
+        currentTime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        ret, src = cap.read()
+        img_rot = rotateImg(src)
+        img = cv2.resize(img_rot, (224,224))
+        imgpred = np.expand_dims(img, axis=0)  # Add a batch dimension
+        pred = model.predict(imgpred).flatten()
+        pred = tf.nn.sigmoid(pred)
+        pred = tf.where(pred < 0.5, 0, 1)
+        cv2.imshow("Camera", src)
+        # print("pred: ", pred[0])
+        if pred[0] == 1:
+            print("ID Card detected! Saving it to PC!")
+            try:
+                os.mkdir("pred")
+            except:
+                pass
+            filename = "pred/idpred_{date}.jpg".format(date=currentTime)
+            # print(filename)
+            cv2.imwrite(filename, img_rot)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            # exit()
+            break
+
+
+if __name__ == "__main__":
+    # rotateImg(document_img)
+    evaluate()
---
+++ model.py
@@ -0,0 +1,104 @@
+import tensorflow as tf
+import numpy as np
+from main import rotateImg
+from sklearn.model_selection import train_test_split
+import os
+import cv2
+import random
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+def loadData(folder_path):
+    images = []
+    # for folder_path in folder_path_list:
+    i = 0
+    listdir = os.listdir(folder_path)
+    random.shuffle(listdir)
+    for image_name in listdir:
+        if i == 20:
+            break
+        print(image_name)
+        image_path = os.path.join(folder_path, image_name)
+
+        # Load the image and preprocess it if needed
+        image = cv2.imread(image_path)
+        # apply rotation
+        image = rotateImg(image)
+        # need resizing to standard
+        image = cv2.resize(image, (224,224))
+        # Append the image and label to the lists
+        images.append(image)
+        i+=1
+
+    return np.array(images)
+
+def trainModel(path_pos_img, path_neg_img):
+    pos_data = loadData(path_pos_img)
+    pos_label = np.ones(pos_data.shape[0])
+    neg_data = loadData(path_neg_img)
+    neg_label = np.zeros(neg_data.shape[0])
+
+    X = np.concatenate([pos_data, neg_data])
+    y = np.concatenate([pos_label, neg_label])
+    random_seed = 42
+    np.random.seed(random_seed)
+
+    # Shuffle the data
+    shuffle_indices = np.random.permutation(len(X))
+    X = X[shuffle_indices]
+    y = y[shuffle_indices]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=random_seed)
+    train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+    test_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+
+    BATCH_SIZE = 8
+    SHUFFLE_BUFFER_SIZE = 100
+
+    train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+    test_dataset = test_dataset.batch(BATCH_SIZE)
+
+    data_augmentation = tf.keras.Sequential([
+      tf.keras.layers.RandomFlip('horizontal'),
+      tf.keras.layers.RandomRotation(0.2),
+    ])
+    # IMG_SHAPE = IMG_SIZE + (3,)
+    base_model = tf.keras.applications.MobileNetV2(input_shape=(224,224,3),
+                                                   include_top=False,
+                                                   weights='imagenet')
+    base_model.trainable = False
+
+    inputs = tf.keras.Input(shape=(224, 224, 3))
+    x = data_augmentation(inputs)
+    x = tf.keras.applications.mobilenet_v2.preprocess_input(x)
+    x = base_model(x, training=False)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    outputs = tf.keras.layers.Dense(1)(x)
+    model = tf.keras.Model(inputs, outputs)
+
+    base_learning_rate = 0.0001
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
+                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0, name='accuracy')])
+
+    model.fit(train_dataset,epochs=20)
+
+    # Retrieve a batch of images from the test set
+    image_batch, label_batch = test_dataset.as_numpy_iterator().next()
+    predictions = model.predict_on_batch(X_test).flatten()
+
+    # Apply a sigmoid since our model returns logits
+    predictions = tf.nn.sigmoid(predictions)
+    predictions = tf.where(predictions < 0.5, 0, 1)
+
+    print('Predictions:\n', predictions.numpy())
+    print('Labels:\n', y_test)
+
+    model.save("mobilenetv2ID.keras")
+    print("Training done and model saved!!")
+    cm = confusion_matrix(y_test, predictions)
+    print(cm)
+    cr = classification_report(y_test, predictions)
+    print(cr)
+
+trainModel("id", "../img_celeba/imagenette2-320/train")
